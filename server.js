ac4213913b6ed7f2764c879d11460de53c8f1aa4
amin_ruhul <ruhulinfo50@gmail.com>
2021-09-23 16:45:53 +0600

    handle unhandle promise rejection

--- server.js
+++ server.js
@@ -16,6 +16,14 @@ app.use("/api", productRoute);
 
 // const config server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () =>
+const server = app.listen(PORT, () =>
   console.log(`server running at port:${PORT} in ${process.env.NODE_ENV} mode`)
 );
+
+// handle unhandle promise rejection
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+
+  // exit the server
+  server.close(() => process.exit(1));
+});
