f4d713423edcf5a4dbdab840aed567c52ecc000c
amin_ruhul <ruhulinfo50@gmail.com>
2021-09-23 15:41:50 +0600

    create server

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require("express");
+const dotenv = require("dotenv");
+
+// load config file
+dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
 // const config server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`server running at port:${PORT}`));
+app.listen(PORT, () =>
+  console.log(`server running at port:${PORT} in ${process.env.NODE_ENV} mode`)
+);
