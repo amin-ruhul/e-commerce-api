f0a7ecd20b18e88acea64bb2b2625300903e0ecd
amin_ruhul <ruhulinfo50@gmail.com>
2021-10-06 11:37:16 +0600

    create my order and single order function

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -2,6 +2,7 @@ const Order = require("../models/Order");
 const Product = require("../models/Product");
 const ErrorHandler = require("../utils/ErrorHandler");
 
+// crate new order
 const newOrder = async (req, res, next) => {
   try {
     const order = await Order.create({
@@ -19,6 +20,40 @@ const newOrder = async (req, res, next) => {
   }
 };
 
+// get single order
+const singleOrder = async (req, res, next) => {
+  try {
+    const order = await Order.findById(req.params.id).populate(
+      "user",
+      "name email"
+    );
+
+    if (!order) return next(new ErrorHandler("Order Not found", 404));
+
+    res.status(200).json({
+      success: true,
+      order,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// get logged in user order
+const myOrder = async (req, res, next) => {
+  try {
+    const orders = await Order.find({ user: req.user._id });
+    res.status(200).json({
+      success: true,
+      orders,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
 module.exports = {
   newOrder,
+  singleOrder,
+  myOrder,
 };
--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -1,10 +1,20 @@
 const express = require("express");
-const { newOrder } = require("../controllers/orderController");
+const {
+  newOrder,
+  singleOrder,
+  myOrder,
+} = require("../controllers/orderController");
 const route = express.Router();
 
 const { protect, authorizeRole } = require("../middlewares/auth");
 
 // create new order
 route.post("/new", protect, newOrder);
+
+// logged in user orders
+route.get("/myorder", protect, myOrder);
+
+// single order
+route.get("/:id", protect, singleOrder);
 
 module.exports = route;
