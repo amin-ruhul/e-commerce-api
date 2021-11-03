ea94aebedcbc9c2822381a49c99df34af2ab1f06
amin_ruhul <ruhulinfo50@gmail.com>
2021-11-03 09:22:01 +0600

    add cors middleware

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const productRoute = require("./routes/productRoute");
 const userRoute = require("./routes/userRoute");
@@ -19,6 +20,7 @@ dotenv.config({ path: "./config/config.env" });
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors());
 
 // database connection
 DB_connection();
