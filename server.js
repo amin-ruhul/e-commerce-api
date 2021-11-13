ad81e7ada2557abb13b1b5f602ee65c13069b6fb
amin_ruhul <ruhulinfo50@gmail.com>
2021-11-13 08:29:02 +0600

    change body parser

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
-//const bodyParser = require("body-parser");
 const cloudinary = require("cloudinary");
 const fileUpload = require("express-fileupload");
 const cookieParser = require("cookie-parser");
@@ -22,7 +21,7 @@ process.on("uncaughtException", (err) => {
 dotenv.config({ path: "./config/config.env" });
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(fileUpload);
 app.use(cors());
